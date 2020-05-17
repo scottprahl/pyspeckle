bcabb8ad1e400a25f608a2ce9a20e3fbf5664796
Scott Prahl <scott@prahls.com>
2020-05-17 08:51:49 -0700

    sphinxing

---
+++ README.rst
@@ -0,0 +1,37 @@
+pyspeckle
+=========
+
+A basic collection of routines to track and analyze speckle.  This is a python
+version of SimSpeckle Matlab Package by Duncan and Kirkpatrick.
+
+Currently incomplete and untested
+
+Usage
+--------
+
+For examples, see the docs directory
+
+Installation
+------------
+
+The `pyspeckle` package can be installed via `pip`::
+
+    pip install pyspeckle
+
+Alternatively you can install from github
+
+    git clone https://github.com/scottprahl/pyspeckle.git
+
+and add the `pyspeckle` directory to your PYTHONPATH
+
+
+Dependencies
+------------
+
+Required Python modules: numpy, matplotlib, scipy
+
+
+License
+-------
+
+pyspeckle is licensed under the terms of the MIT license.
--- setup.py
+++ setup.py
@@ -1,35 +1,13 @@
-"""
-Copyright 2018 Scott Prahl
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-"""
-
 from setuptools import setup
 
+# use README as the long description
+# make sure to use the syntax that works in both ReST and markdown
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-	name='pyspeckle',
-	packages=['pyspeckle'],
-	version='0.2.0',
-	description='Routines for analysis of laser speckle',
-	url='https://github.com/scottprahl/pyspeckle.git',  
-	author='Scott Prahl',
-	author_email='scott.prahl@oit.edu',
-	license='MIT',
-	classifiers=[
-		'Development Status :: 3 - Alpha',
-		'License :: OSI Approved :: MIT License',
-		'Intended Audience :: Science/Research',
-		'Programming Language :: Python',
-		'Topic :: Scientific/Engineering :: Physics',
-	],
-	keywords=['objective speckle', 'subjective speckle', 'speckle contrast', 'speckle size'],
-	install_requires=['numpy','matplotlib'],
-	long_description=
-	"""
-	Adaptation of SimSpeckle routines by Duncan and Kirkpatrick to python.
-	""",
+    long_description=long_description,
+    long_description_content_type='text/x-rst'
 )
